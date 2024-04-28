26eca7d0ebf0083cb290ec9990244f7d6c00257d
wpnikita <wpnikita206@gmail.com>
2024-04-28 14:16:43 +0530

    11 April 2024

---
+++ src/ex_11042024/Lab002.java
@@ -0,0 +1,10 @@
+package ex_11042024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hi There!"); //println is for new line
+        System.out.print("Hi Nikita ");  //not new line
+        System.out.println("Prathmesh");
+
+    }
+}
