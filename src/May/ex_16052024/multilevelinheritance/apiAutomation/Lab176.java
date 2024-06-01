e12b5ab0c075faa43d100f58db8c622eb19554e3
wpnikita <wpnikita206@gmail.com>
2024-06-01 20:16:58 +0530

    16 May 2024

--- src/May/ex_16052024/multilevelinheritance/apiAutomation/Lab176.java
+++ src/May/ex_16052024/multilevelinheritance/apiAutomation/Lab176.java
@@ -1,6 +1,6 @@
 package may.ex_16052024.multilevelinheritance.apiAutomation;
 
-import src.may.ex_16052024.singleinheritance.pramodHome.Father;
+import may.ex_16052024.singleinheritance.pramodHome.Father;
 
 public class Lab176 extends Father {
     void house(){
